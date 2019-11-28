2300b8cbf9e7fe301801729b29e3a604697b0b75
oussou94 <waz26394@gmail.com>
2019-11-28 02:19:17 +0000

    commit lesson

---
+++ conditions.php
@@ -0,0 +1,25 @@
+<?php 
+	$age = 9;
+	/*if ($age <= 12) {
+		echo "salut gamin, bienvenue sur mon site <br />";
+		$autorisation_entrer = "oui";
+	}
+	else
+	{
+		echo "ce site est un site pour enfant, vous etes trop vieux pour entrer.Au revoir!<br />";
+		$autorisation_entrer = "non";
+
+	}
+	echo "Avez vous l'autorisation d'entrer?<br/> la reponse est : $autorisation_entrer";*/
+
+	$langue = "anglais";
+	if ($age <= 12 and $langue == "francais") {
+		echo "Bienvenue sur mon site !";
+	}
+	elseif ($age <= 12 and $langue == "anglais" ) {
+		echo "welcome to my site!";
+	}
+
+
+
+ ?>
---
+++ variables.php
@@ -0,0 +1,11 @@
+<?php 
+	$age = 15;
+	$chaine = "une chaine de caractere";
+	$boul = true;
+	echo 'il a '.$age.' ans';?><br/><?php
+	$modulo = 5 % 2 ;
+	echo 'le reste de la division est'. $modulo;
+	
+
+
+ ?>
