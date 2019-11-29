fe2a73a26a3a759a517c192099953ed9305766be
oussou94 <waz26394@gmail.com>
2019-11-29 00:26:53 +0000

    les boucles

---
+++ boucles.php
@@ -0,0 +1,12 @@
+<?php 
+/*$nombre_de_ligne = 1;
+
+while ($nombre_de_ligne <= 10) {
+	echo 'ligne numero_'. $nombre_de_ligne.'<br />';
+	$nombre_de_ligne++;
+}*/
+for($nombre_de_ligne = 1;$nombre_de_ligne <= 10;$nombre_de_ligne++){
+	echo 'ligne numero '.$nombre_de_ligne.'<br />';
+}
+//les deux boucles font la meme chose
+ ?>
